93b0e7ee2ffeb2aaf96ea39023e57982834d6ba6
sonalihotwani <hotwanisonali09@gmail.com>
2025-01-09 18:35:08 +0530

    Changes the login logic to check the username password stored in the DB

--- login-page.js
+++ login-page.js
@@ -3,19 +3,36 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 // When the login button is clicked, the following code is executed
-loginButton.addEventListener("click", (e) => {
+loginButton.addEventListener("click", async (e) => {
     // Prevent the default submission of the form
     e.preventDefault();
     // Get the values input by the user in the form fields
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        // If the credentials are valid, show an alert box and reload the page
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        // Otherwise, make the login error message show (change its oppacity)
-        loginErrorMsg.style.opacity = 1;
+    try {
+        const data = {};
+        data['username'] = username;
+        data['password'] = password;
+        const response = await fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            alert('Registration successful: ' + result.message);
+        } else {
+            const error = await response.json();
+            // Otherwise, make the login error message show (change its oppacity)
+            loginErrorMsg.style.opacity = 1;
+            alert('Error: ' + (error.error || 'Something went wrong'));
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error: Could not connect to the server.');
     }
 })
